326958e87b94cae120079b28bd5a44f66c2ce3d4
Isac Vieira <isacvieira2009@hotmail.com>
2024-03-05 21:25:46 -0300

    Changes

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import face_recognition as fr
 import os
 import shutil
 
-img_base = fr.load_image_file('123456.jpg')
+img_base = fr.load_image_file('123456.jpg')# Carregue sua foto base para o reconhecimento
 img_base = cv2.cvtColor(img_base, cv2.COLOR_BGR2RGB)
 faceLoc = fr.face_locations(img_base)[0]
 encodeBase = fr.face_encodings(img_base)[0]
 
 
-directory_base = 'C:/Users/isacv/OneDrive/Área de Trabalho/Teste'
-directory_dest = 'C:/Users/isacv/OneDrive/Área de Trabalho/fotos-mae'
+directory_base = 'C:/Users/isacv/OneDrive/Área de Trabalho/Teste'# Diretorio origem que o algoritimo ira percorrer
+directory_dest = 'C:/Users/isacv/OneDrive/Área de Trabalho/foto' # Diretorio para qual as fotos  compatíveis com a base serão enviadas
 
 #Percorre diretório raiz
 for pic_name in os.listdir(directory_base):
@@ -22,14 +22,14 @@ for pic_name in os.listdir(directory_base):
     try:
         pic_act_encode = fr.face_encodings(pic_act)[0]
     except IndexError:
-        print('I not find a face')
+        print('Não consegui localizar um rosto')
         continue
 
-    compare_pic = fr.compare_faces([encodeBase], pic_act_encode)
+    compare_pic = fr.compare_faces([encodeBase], pic_act_encode) #Compara a foto atual com a foto base
 
-    if compare_pic[0]:
+    if compare_pic[0]: #Se p resultado da comparação acima retornar "true", move a foto para o novo diretorio
         shutil.move(pic_in_path, directory_dest)
-        print('Picture moved')
+        print('Foto compatível, movendo para o novo diretório...')
     else:
         print('Foto não corresponde a base')
 cv2.waitKey(0)
